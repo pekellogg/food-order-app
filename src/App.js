f355473d2355f3a095c4e29af6e154091f4c75e2
pekellogg <pekellogg@hotmail.com>
2023-01-09 16:26:09 -0800

    added Meals and Cart components to main

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import { useState } from "react";
+import Cart from "./components/cart/Cart";
 import Header from "./components/layout/Header";
 import Meals from "./components/meals/Meals";
 
 function App() {
 
+  const [cartVisibility, setCartVisibility] = useState(false);
+
+  const displayCartHandler = () => {
+    setCartVisibility(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartVisibility(false);
+  };
+
   return (
     <>
-      <Header />
+      {cartVisibility && <Cart onClose={hideCartHandler}/>}
+      <Header onDisplayCart={displayCartHandler}/>
       <main>
         <Meals />
       </main>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -10,7 +10,7 @@ const Header = (props) => {
         <h1>
             ReactMeals
         </h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onDisplayCart}/>
       </header>
       <div className={classes["main-image"]}>
         <img
